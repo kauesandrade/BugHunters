1e50db23650b64690aea342c28b42cef9be9cba2
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2025-04-15 20:20:35 -0300

    finish Header

--- src/app/atividades/page.tsx
+++ src/app/atividades/page.tsx
@@ -1,9 +1,6 @@
-import Header from "@/components/header";
-
 const Atividades = () => {
     return (
         <>
-        <Header/>
         </>
     )
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,34 @@
+'use client';
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
 const Header = () => {
+
+    const router = useRouter();
+
+    const handleNavigation = (path: string) => {
+        router.push(path);
+    }
+
     return (
         <header className="grid grid-flow-col px-18 py-8 shadow-md shadow-orange">
             <section className="flex items-center">
-                <img src="/public/logo.svg" alt="Logo Bug Hunters"/>
+                <img className="cursor-pointer" onClick={() => handleNavigation("/")} src="/logo.svg" alt="Logo Bug Hunters"/>
             </section>
-            <section className="flex items-center font-poppins">
-                <ul className="flex gap-12">
+            <section className="flex items-center font-extralight">
+                <ul className="flex gap-24">
                     <li>
-                        <a className="text-white font-poppins font-extralight" href="">Conteúdos</a>
+                        <a className="cursor-pointer" onClick={() => handleNavigation("/conteudos")}>Conteúdos</a>
                     </li>
                     <li>
-                        <a href="">Atividades</a>
+                        <a className="cursor-pointer" onClick={() => handleNavigation("/atividades")}>Atividades</a>
                     </li>
                     <li>
-                        <a href="">Sobre</a>
+                        <a className="cursor-pointer" onClick={() => handleNavigation("/sobre")}>Sobre</a>
                     </li>
                     <li>
-                        <a href="">Contato</a>
+                        <a className="cursor-pointer" onClick={() => handleNavigation("/contato")}>Contato</a>
                     </li>
                 </ul>
             </section>
