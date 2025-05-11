c2cde17ed8914ac3132ab429ea495f7ac550c9ca
João Vitor Winter <joaovwinter3108@gmail.com>
2025-05-11 12:46:42 -0300

    Meio que fiz é isso ai | 11/05 12:46

---
+++ src/components/CodeBlock/index.tsx
@@ -0,0 +1,32 @@
+'use client'
+import 'highlight.js/styles/github-dark.css';
+import hljs from 'highlight.js/lib/core'
+import c from 'highlight.js/lib/languages/c'
+import { useEffect, useRef } from 'react';
+hljs.registerLanguage('c', c)
+
+type CodeBlockProps = {
+    code: string;
+}
+
+const CodeBlock = ({code}: CodeBlockProps) => {
+    const codeRef = useRef(null);
+    // const highlightedCode = hljs.highlight(code, { language: 'c' }).value
+    
+    useEffect(() => {
+        if (!codeRef.current) return;
+
+        hljs.highlightElement(codeRef.current);
+    }, [])
+    return (
+        <pre className="mt-14 px-14 py-10 rounded-3xl bg-[#27242C]">
+            <code className={"!bg-[inherit] hljs language-c"} ref={codeRef} 
+            // dangerouslySetInnerHTML={{__html: highlightedCode}}
+            >
+                {code}
+            </code>
+        </pre>
+    )
+}
+
+export default CodeBlock;
