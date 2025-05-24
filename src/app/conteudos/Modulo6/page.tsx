4fc4b0fc0660fcddcc6dcf1622065bc6e20fd227
bino1432 <bernardozplz@gmail.com>
2025-05-23 22:29:19 -0300

    Module6 page finished

--- src/app/conteudos/Modulo6/page.tsx
+++ src/app/conteudos/Modulo6/page.tsx
@@ -1,3 +1,5 @@
+import CodeBlock from "@/components/CodeBlock";
+
 const modulo6 = () => {
     return(
         <main>
@@ -52,6 +54,104 @@ const modulo6 = () => {
                             Apartir das alterações alterações dentro do código, o valor condicional deve ser alterado a 
                             fim de evitar "loops infinitos" e como consequências travar o programa.
                         </p>
+                        <p className="mt-4 underline underline-offset-4 decoration-orange">
+                            Exemplo while:
+                        </p>
+                        <CodeBlock code={`
+int main(){ 
+    // Contador de numeros até 10 
+
+    // Contador com o nome de "i"
+    int i = 0 
+
+    // Enquanto contador for menor que 10
+    while(i <= 10){ 
+
+    // Escreve o numero
+    printf(“%i”, i); 
+
+    // Soma mais 1 ao "i"
+    i++; 
+    } 
+
+    return 0; 
+
+} 
+                            `} />
+                    </div>
+                    <div>
+                        <h4 className="font-medium text-xl underline underline-offset-4 decoration-orange">
+                            1 - Do While
+                        </h4>
+                        <p className="mt-4">
+                           O "Do While" funciona da mesma forma que um "while" normal, mas diferentemente do "while" que primeiro faz a 
+                           verificação da condição antes de rodar o código, o "Do While" primeiro roda o 
+                           código e em seguida faz a verificação. 
+                        </p>
+                        <p className="mt-4 underline underline-offset-4 decoration-orange">
+                            Exemplo Do while:
+                        </p>
+                        <CodeBlock code={`
+int main(){ 
+    // contador de numeros até 10 
+
+    // Contador com o nome de "i"
+    int i = 0 
+    
+    // Roda o bloco de código que está dentro da estrutura de repetição
+    do{ 
+
+    printf(“%i”, i); 
+
+    i++; 
+
+    // Faz a verificação da condição
+    } while(i < 10); 
+
+return 0; 
+
+} 
+                            `} />
+                    </div>
+                    <div>
+                        <h4 className="font-medium text-xl underline underline-offset-4 decoration-orange">
+                            1 - For
+                        </h4>
+                        <p className="mt-4">
+                            O for tem uma estrutura um pouco mais complexa que o While e Do While, ele possui três pilares  
+                        </p>
+                        <div className="mt-4">
+                            <p>
+                               1 – Uma variável contadora,  
+                            </p>
+                            <p>
+                               2 – Uma condição baseada na variável,
+                            </p>
+                            <p>
+                                3 – Um pedaço de código que é executado ao final de cada repetição   
+                            </p>
+                        </div>
+                        <p className="mt-4">
+                            Além disso, já que ele possui uma sintaxe mais rígida, é difícil ter problemas com “loop infinito”. 
+                        </p>
+                        <p className="mt-4 underline underline-offset-4 decoration-orange">
+                            Exemplo for:
+                        </p>
+                        <CodeBlock code={`
+int main(){ 
+    // contador de numeros até 10 
+
+    // sintaxe basica do for
+    for(int i = 0; i <= 10; i++){ 
+
+    printf(“%i”, i); 
+
+    } 
+
+return 0; 
+
+}  
+                            `} />
                     </div>
                 </div>
             </section>
